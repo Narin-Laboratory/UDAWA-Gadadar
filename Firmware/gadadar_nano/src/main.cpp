eb390a189561ae42900355f49c34f203bbf48391
iwas108 <64204327+iwas108@users.noreply.github.com>
2023-01-25 22:36:17 +0800

    wip

--- Firmware/gadadar_nano/src/main.cpp
+++ Firmware/gadadar_nano/src/main.cpp
@@ -1,8 +1,6 @@
 #include "main.h"
 
 libudawaatmega328 nano;
-SoftwareSerial pzemSWSerial(S1_RX, S1_TX);
-PZEM004Tv30 PZEM(pzemSWSerial);
 Settings mySettings;
 
 void setup() {
@@ -19,30 +17,10 @@ void loop() {
   if(doc["err"] == nullptr && doc["err"].as<int>() != 1)
   {
     const char* method = doc["method"].as<const char*>();
-    if(strcmp(method, (const char*) "setSettings") == 0)
+    if(strcmp(method, (const char*) "sStng") == 0)
     {
 
     }
-    else if(strcmp(method, (const char*) "getPowerUsage") == 0){
-        getPowerUsage(doc);
-    }
   }
   doc.clear();
-}
-
-void getPowerUsage(StaticJsonDocument<DOCSIZE> &doc){
-  float volt = PZEM.voltage();
-  if(volt > 0){
-    doc["volt"] = PZEM.voltage();
-    doc["amp"] = PZEM.current();
-    doc["watt"] = PZEM.power();
-    doc["ener"] = PZEM.energy();
-    doc["freq"] = PZEM.frequency();
-    doc["pf"] = PZEM.pf();
-
-    nano.serialWriteToESP32(doc);
-  }
-  else{
-    doc["err"] = 1;
-  }
 }
--- Firmware/gadadar_nano/src/main.h
+++ Firmware/gadadar_nano/src/main.h
@@ -1,14 +1,7 @@
+#define DOCSIZE 384
 #include <libudawaatmega328.h>
-#include <PZEM004Tv30.h>
-#include <SoftwareSerial.h>
-
-#define S1_RX 11
-#define S1_TX 12
 
 struct Settings
 {
 
 };
-
-
-void getPowerUsage(StaticJsonDocument<DOCSIZE> &doc);
