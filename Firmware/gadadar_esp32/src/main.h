209b41553869ce35c244dbe91e9a5f95fc000a7e
iwas108 <64204327+iwas108@users.noreply.github.com>
2023-02-05 10:38:31 +0800

    v0.0.3

--- Firmware/gadadar_esp32/src/main.h
+++ Firmware/gadadar_esp32/src/main.h
@@ -59,7 +59,7 @@ ny6l9/duT2POAsUN5IwHGDu8b2NT+vCUQRFVHY31
 #include <Adafruit_BME280.h>
 
 #define CURRENT_FIRMWARE_TITLE "Gadadar"
-#define CURRENT_FIRMWARE_VERSION "0.0.2"
+#define CURRENT_FIRMWARE_VERSION "0.0.3"
 
 const char* settingsPath = "/settings.json";
 struct Settings
