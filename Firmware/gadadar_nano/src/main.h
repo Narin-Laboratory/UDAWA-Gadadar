7f74cb5f54da8de61058d9a36f52d00eb1f71fd8
iwas108 <64204327+iwas108@users.noreply.github.com>
2023-02-23 11:11:24 +0800

    Updated header

--- Firmware/gadadar_nano/src/main.h
+++ Firmware/gadadar_nano/src/main.h
@@ -1,3 +1,11 @@
+/**
+ * UDAWA - Universal Digital Agriculture Watering Assistant
+ * Firmware for Actuator 4Ch UDAWA Co-MCU Board (Gadadar)
+ * Licensed under aGPLv3
+ * Researched and developed by PRITA Research Group & Narin Laboratory
+ * prita.undiknas.ac.id | narin.co.id
+**/
+
 #define DOCSIZE 384
 #include <libudawaatmega328.h>
 
